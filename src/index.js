7c6481b65508ef2bd5d16e4c66a430eadf355699
may-studio <>
2020-11-25 19:14:58 +0300

    fixing bugs

--- src/index.js
+++ src/index.js
@@ -3,20 +3,6 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import * as firebase from "firebase/app";
-
-var firebaseConfig = {
-  apiKey: "AIzaSyDHhwCoRqzrUfD99gXmZCUmXSI1sNOuk-0",
-  authDomain: "may-shop.firebaseapp.com",
-  databaseURL: "https://may-shop.firebaseio.com",
-  projectId: "may-shop",
-  storageBucket: "may-shop.appspot.com",
-  messagingSenderId: "1076349410569",
-  appId: "1:1076349410569:web:78cce878db584a90c4c7d8",
-  measurementId: "G-DJ0CPHLSSE"
-};
-
-firebase.initializeApp(firebaseConfig);
 
 ReactDOM.render(
   <React.StrictMode>
