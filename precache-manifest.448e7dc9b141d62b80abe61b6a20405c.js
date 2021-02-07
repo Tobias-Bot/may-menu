38b980a12e7e7cbf8840e078c49d3c098e2d8758
may-studio <>
2021-02-07 17:09:33 +0300

    Updates

---
+++ precache-manifest.448e7dc9b141d62b80abe61b6a20405c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8d78f38517f26cbe34bb6c5bbbddefa2",
+    "url": "/may-menu/index.html"
+  },
+  {
+    "revision": "9bf3b6890b37498bc78b",
+    "url": "/may-menu/static/css/main.fff6af3e.chunk.css"
+  },
+  {
+    "revision": "a35642030306a724e326",
+    "url": "/may-menu/static/js/2.3b94c1a9.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/may-menu/static/js/2.3b94c1a9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9bf3b6890b37498bc78b",
+    "url": "/may-menu/static/js/main.b371358f.chunk.js"
+  },
+  {
+    "revision": "eac62d8f6ef33c9a71bf",
+    "url": "/may-menu/static/js/runtime-main.ff71d5d6.js"
+  },
+  {
+    "revision": "c88cdb0e2715771553e34cdc14a19a14",
+    "url": "/may-menu/static/media/logo.c88cdb0e.png"
+  }
+]);
